4659b96c83fec0f9ebfc4a6b2e8b73f7a8ee27fb
bhargavijyo <bhargavasuthi@gmail.com>
2026-08-13 10:28:39 +0530

    upadated code logic

---
+++ index.js
@@ -0,0 +1,78 @@
+//console.log("Welcome to javascript");
+
+/*var name = "Javascript";
+var name = "Python";
+console.log(name);*/
+
+/*let age = 21;
+age = 29;
+console.log(age);*/
+
+//let n = "Js";
+/*let s = "python";
+let n="hasini";
+console.log(s);*/
+
+/*const p = 3.14;
+const p =3;
+console.log(p);*/
+
+
+/*let s = "Node.js";
+var m = "python";
+var m = "idiot";
+
+const pi = 3.14;
+
+console.log(s);
+console.log(m);
+console.log(pi);*/
+
+/*let v= 30;
+console.log(v);*/
+/*let name = "Ravi";
+let age = 69;
+console.log(name);
+console.log(age);*/
+
+/*let value = alert("Are you student or job holder? ");
+console.log(value);
+
+let v = confirm("Are you sure you want to learn and grow in carrer?");
+console.log(v);
+
+let result = confirm("Do you want to logout?");
+console.log(result);*/
+
+/*let r = confirm("Are you sure you want to delete this file?");
+if(r){
+    console.log("File deleted");
+}else{
+    console.log("Delete cancelled");
+}*/
+
+/*let name = prompt("Enter your name:");
+alert("Welcome " +name);
+console.log(name);*/
+
+/*let n = document.writeln("Hello, welcome to javascript");
+console.log(n);
+
+let name = "Ajesh";
+let res =document.writeln("<br> welcome " +name);
+console.log(res);*/
+
+ /*console.log("Javascript started");
+ alert("Welcome!");
+ let name = prompt("Enter your name?");
+ let result = confirm("Do you want to continue?");
+
+ console.log("Name ",name);
+ console.log("Continue:", result);
+ document.writeln("Hello " +name);
+*/
+
+console.error("Something wrong!");
+console.warn("warning");
+console.clear();
+
